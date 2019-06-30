a38dca5f6eb84ef597091c7940456ba05292131d
xoltia <38849891+xoltia@users.noreply.github.com>
2019-06-29 20:50:53 -0500

    Add kick and ban commands

---
+++ VanillaBot/Modules/AdminCommands.cs
@@ -0,0 +1,48 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanillaBot.Modules
+{
+    [Name("admin")]
+    public class AdminCommands : ModuleBase<SocketCommandContext>
+    {
+        // TODO: add reasons
+
+        [Command("ban")]
+        [Alias("banish", "removeof", "destroy")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        public async Task Ban(IGuildUser member)
+        {
+            if (Context.Guild.Owner.Id == member.Id)
+            {
+                await ReplyAsync("You can't ban the guild owner!");
+                return;
+            }
+
+            await member.BanAsync();
+            await ReplyAsync("I didn't like that guy anways.");
+        }
+
+        [Command("kick")]
+        [Alias("showout")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        public async Task Kick(IGuildUser member)
+        {
+            if (Context.Guild.Owner.Id == member.Id)
+            {
+                await ReplyAsync("You can't kick the guild owner!");
+                return;
+            }
+
+            await member.BanAsync();
+            await ReplyAsync($"Later bud {member.Mention}");
+        }
+    }
+}
