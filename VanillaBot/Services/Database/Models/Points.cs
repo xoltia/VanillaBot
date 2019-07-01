c71ad8b01dcd5c473706a8d968faba8292baf6c9
xoltia <38849891+xoltia@users.noreply.github.com>
2019-07-01 07:51:47 -0500

    Use user id as key for points

--- VanillaBot/Services/Database/Models/Points.cs
+++ VanillaBot/Services/Database/Models/Points.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace VanillaBot.Services.Database.Models
 {
     public class Points
     {
-        public int Id { get; set; }
+        [Key]
         public string UserId { get; set; }
         public int Amount { get; set; }
     }
